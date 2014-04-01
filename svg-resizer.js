adb73f3877789f8ba48e03979307609a9e6dd763
vieron <javiersanchezmarin@gmail.com>
2014-04-02 00:15:56 +0200

    feat: pt to px conversion

--- svg-resizer.js
+++ svg-resizer.js
@@ -5,7 +5,15 @@ var path = require('path');
 var fs = require('fs-extra');
 var _ = require('lodash');
 var xml2js = require('xml2js');
+var builder = new xml2js.Builder();
 var parseString = xml2js.parseString;
+
+var ptToPx = function(pt) {
+    return pt * dpi * (1 / 72);
+};
+var pxToPt = function(px) {
+    return px / (dpi * (1 / 72));
+};
 
 
 if (!which('rsvg-convert')) {
@@ -44,10 +52,12 @@ var opts = require("nomnom")
 // create output folder if dont exist
 mkdir('-p', path.join(opts.output));
 
+var dpi = 96;
 var svgFiles = opts._;
-var finalWidth = opts.width || opts.height;
-var finalHeight = opts.height || opts.width;
+var finalWidth = pxToPt(opts.width || opts.height);
+var finalHeight = pxToPt(opts.height || opts.width);
 var finalRatio = finalWidth / finalHeight;
+
 
 svgFiles.forEach(function(svgPath) {
 
@@ -57,18 +67,20 @@ svgFiles.forEach(function(svgPath) {
         var fileContent = fs.readFileSync(svgPath, 'utf8');
 
         parseString(fileContent, function (err, parsedFileContent) {
-            origWidth = parseInt(parsedFileContent.svg.$.width, 10);
-            origHeight = parseInt(parsedFileContent.svg.$.height, 10);
+            origWidth = pxToPt(parseInt(parsedFileContent.svg.$.width, 10));
+            origHeight = pxToPt(parseInt(parsedFileContent.svg.$.height, 10));
         });
 
         var origRatio = origWidth / origHeight;
 
         if (origRatio < finalRatio) {
             newHeight = finalHeight;
-            newWidth = Math.floor(origWidth / (origHeight / newHeight));
+            newWidth = origWidth / (origHeight / newHeight);
+            newWidth = Math.floor(newWidth);
         } else {
             newWidth = finalWidth;
-            newHeight = Math.floor(origHeight / (origWidth / newWidth));
+            newHeight = origHeight / (origWidth / newWidth);
+            newHeight = Math.floor(newHeight);
         }
 
         opts.width = newWidth;
@@ -82,19 +94,41 @@ svgFiles.forEach(function(svgPath) {
         opts.width ? '-w ' + opts.width : null,
         opts.height ? '-h ' + opts.height : null,
         '--keep-aspect-ratio',
-        '--dpi-x 72', // work with pixels
-        '--dpi-y 72', // work with pixels
+        '--dpi-x 90', // work with pixels
+        '--dpi-y 90', // work with pixels
         '-f ' + opts.format,
         svgPath,
-        opts.output ? '-o ' + outputPath : null
+        '-o ' + outputPath
     ]);
 
+    // print rsvg command
     echo('rsvg-convert ' + args.join(' '));
 
+    // resize file with librsvg
     var convert = exec('rsvg-convert ' + args.join(' '));
+
     if (convert.code !== 0) {
         echo('Error converting file: ' + svgPath);
     }
+
+    // read resized file and change pt to px
+    var resizedFileContent = fs.readFileSync(outputPath, 'utf8');
+
+    parseString(resizedFileContent, function (err, parsedFileContent) {
+        var w = parsedFileContent.svg.$.width;
+        var h = parsedFileContent.svg.$.height;
+
+        w = w.match(/pt$/) ? ptToPx(parseInt(w, 10)) + 'px' : w;
+        h = h.match(/pt$/) ? ptToPx(parseInt(h, 10)) + 'px' : h;
+
+        parsedFileContent.svg.$.width = w;
+        parsedFileContent.svg.$.height = h;
+        // parsedFileContent.svg.$.viewBox = [0, 0, parseInt(w, 10), parseInt(h, 10)].join(' ');
+
+        var finalSVG = builder.buildObject(parsedFileContent);
+        fs.outputFileSync(outputPath, finalSVG);
+    });
+
 });
 
 exit(0);
